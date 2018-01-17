0f1eddd793de5f237e5b9d8f224fb607b12ef45d
cmalfroy <cmalfroy@student.42.fr>
2018-01-17 18:46:32 +0100

    j07

---
+++ j07/ex03/House.class.php
@@ -0,0 +1,7 @@
+<?PHP
+	class House {
+		public function introduce() {
+			printf('House %s of %s : "%s"' . PHP_EOL, $this->getHouseName(), $this->getHouseSeat(), $this->getHouseMotto());
+		}
+	}
+?>
